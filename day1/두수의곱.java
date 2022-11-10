b6bb016d46a918470f3b085446fc1f0ce85b41c2
정정빈 <bini8042@naver.com>
2022-11-10 20:15:02 +0900

    day1

---
+++ day1/두수의곱.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class 두수의곱 {
+
+  public static int solution(int num1, int num2) {
+    int answer = 0;
+
+    answer = num1 * num2;
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solution(3, 7));
+  }
+}
---
+++ day1/두수의합.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class 두수의합 {
+  public static int solution(int num1, int num2) {
+    int answer = -1;
+
+    answer = num1 + num2;
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solution(4, 9));
+  }
+}
