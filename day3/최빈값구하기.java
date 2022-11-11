44b893c0547be64a5f93c3a423e46919f97c17c7
정정빈 <bini8042@naver.com>
2022-11-11 18:59:15 +0900

    day3

--- day3/최빈값구하기.java
+++ day3/최빈값구하기.java
@@ -1,9 +1,9 @@
 public class 최빈값구하기 {
   public static void main(String[] args) {
-    int[] array = { 2, 2, 3, 3 };
+    int[] array = { 2, 2, 3, 3, 3 };
     int[] index = new int[10000]; // 배열값은 크게잡자...런타임에러나서 3시간 낭비했노.. 잊어비리지 말기
 
-    int max = Integer.MIN_VALUE; // 최빈값 몇번들어 갔는지 /임의로 작은수 배정정
+    int count = Integer.MIN_VALUE; // 최빈값 몇번들어 갔는지 /임의로 작은수 배정정
     int mode = 0; // 최빈값
 
     for (int i = 0; i < array.length; i++) {
@@ -11,17 +11,17 @@ public class 최빈값구하기 {
     }
 
     for (int i = 0; i < index.length; i++) {
-      if (max < index[i]) {
-        max = index[i]; // index[3] = 3번
+      if (count < index[i]) {
+        count = index[i]; // index[3] = 3번
         mode = i; // 3
-      } else if (max == index[i]) {
+      } else if (count == index[i]) {
         mode = -1;
       } else if (array.length < 1) {
         mode = array[0];
       }
     }
 
-    // System.out.println(max);
+    // System.out.println(count);
     System.out.println(mode);
   }
 }
