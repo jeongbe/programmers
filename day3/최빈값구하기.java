cd7b553192353c388b700f6074e54b5706995d4f
정정빈 <bini8042@naver.com>
2022-11-12 19:11:27 +0900

    11.12

--- day3/최빈값구하기.java
+++ day3/최빈값구하기.java
@@ -6,7 +6,7 @@ public class 최빈값구하기 {
     int count = Integer.MIN_VALUE; // 최빈값 몇번들어 갔는지 /임의로 작은수 배정정
     int mode = 0; // 최빈값
 
-    for (int i = 0; i < array.length; i++) {
+    for (int i = 0; i < array.length; i++) { // 카운트 하는 부분
       index[array[i]]++;
     }
 
